68167bdfaf1dd8eb9aac5471d3daf1d9689375c1
Dakota Rennemann <rennemannd@gmail.com>
2019-08-10 13:36:31 -0400

    Change to basic multipage TS webapp

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../../logo.svg';
+import './Home.css';
 
-const App: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ const App: React.FC = () => {
   );
 }
 
-export default App;
+export default Home;
