37a30d5653c8428d75177fb06357e3b79aed1f7e
Dakota Rennemann <rennemannd@gmail.com>
2019-09-25 15:33:29 -0400

    Added Header view as part of Home

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes';
 import * as serviceWorker from './serviceWorker';
