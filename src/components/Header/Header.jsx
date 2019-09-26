afa8f057001816df5c47e56c0c224448a3e45d2a
Dakota Rennemann <rennemannd@gmail.com>
2019-09-26 16:09:11 -0400

    Working particle background and updated headerText

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import '../styles/Header.css'
+import '../../styles/Header.css'
 import HeaderText from "./HeaderText";
 import Particles from 'react-particles-js';
+import ParticleParams from '../../docs/particles';
 
 
 const Header = () => {
     return (
-        <div>
-            <div className="Header-particles">
-                <Particles/>
+        <div >
+            <div className="Header-particles noselect">
+                <Particles params={ParticleParams}/>
             </div>
             <HeaderText/>
         </div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './views/Home/Home';
-import Temp from './views/Temp/Temp';
 import NotFound from './views/NotFound';
-
 
 export const Routes = () => {
     return(
         <div>
+
         <Switch>
             <Route exact path="/Home" component={ Home } />
             <Route exact path ="/">
                 <Redirect to="/Home" />
             </Route>
-            <Route exact path ="/Temp" component={ Temp } />
             <Route component={ NotFound } />
         </Switch>
         </div>
