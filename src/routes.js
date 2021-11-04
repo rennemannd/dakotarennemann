d57f2b0d5a551c501f4437ab2b8ee7317ebe3b0c
Dakota Rennemann <rennemannd@gmail.com>
2021-11-04 04:44:23 -0400

    Updated maintenence message

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './views/Home/Home';
 import NotFound from './views/NotFound';
 
 export const Routes = () => {
+    useEffect(() => {
+        alert("This site is currently undergoing temporary maintenance, the full site will be live 11/07 at 12 PM EST");
+    })
+
     return(
         <div>
 
         <Switch>
-            <Route exact path="/Home" component={ Home } />
+            <Route exact path="/Home" component={ NotFound } />
             <Route exact path ="/">
                 <Redirect to="/Home" />
             </Route>
